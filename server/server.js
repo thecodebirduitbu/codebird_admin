1a3d1db4fbd5f9d9c31b8b40139a9cff7f6e5bdc
Puskar-Roy <puskarroy600@gmail.com>
2023-08-16 09:08:42 +0530

    port fix

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 const PORT = 8000 || process.env.PORT;
 
-//------------------------Middlewares--------------------------
+//------------------------Middlewares----------------------
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
