9e10254ec094126604bc99636d0c293037a76c5c
Anuraj kumar <102934270+anurajkumar23@users.noreply.github.com>
2023-08-24 00:29:54 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -12,10 +12,22 @@ dotenv.config();
 const PORT = 8000 || process.env.PORT;
 
 //------------------------Middlewares----------------------
+const allowedOrigins = [
+  process.env.FRONTEND_URL, // No need for interpolation here
+  'https://thecodebird-admin.vercel.app',
+];
+
 const corsOptions = {
-  origin: "https://thecodebird-admin.vercel.app",
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 app.use(cokkie());
 app.use(express.urlencoded({ extended: true }));
--- src/components/helper/helper.js
+++ src/components/helper/helper.js
@@ -1,4 +1,4 @@
-// const url = "http://localhost:8000";  //use when in local server
-const url = "https://thecodebird-admin-server.vercel.app";
+const url = "http://localhost:8000";  //use when in local server
+// const url = "https://the-codebird-website-admin.onrender.com";
 
 export default url
